ee57c557fbebdfcfd8e6a3514aaaaccc5467c7e4
Nate Boyette <nateboyette@icloud.com>
2019-02-20 17:22:53 -0800

    Add endpoint to post a new cohort to the cohort table

--- api/cohortRouter.js
+++ api/cohortRouter.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/helpers/cohortsModel')
+const db = require("../data/helpers/cohortsModel");
 
 const router = express.Router();
 
 //GET
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   db.get()
-  .then(cohorts => {
-    res.status(200).json(cohorts)
-  })
-  .catch(error => {
-    res.status(500).json({
-      error: "There was an error retrieving cohorts data."
+    .then(cohorts => {
+      res.status(200).json(cohorts);
     })
-  })
-})
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error retrieving cohorts data."
+      });
+    });
+});
 
 router.get("/:id", (req, res) => {
-  const id = req.params.id
-  
+  const id = req.params.id;
+
   db.getById(id)
     .then(cohort => {
       res.status(200).json(cohort);
@@ -32,10 +32,8 @@ router.get("/:id", (req, res) => {
     });
 });
 
-
-
 router.get("/:id/students", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   db.getStudentsInCohort(id)
     .then(students => {
@@ -43,13 +41,36 @@ router.get("/:id/students", (req, res) => {
     })
     .catch(error => {
       res.status(500).json({
-        errorMessage: "There was an error retrieving cohorts data.", 
+        errorMessage: "There was an error retrieving cohorts data.",
         error: error
       });
     });
 });
 
+// POST
 
+router.post("/", (req, res) => {
+  const { name } = req.body;
+  const post = req.body;
 
+  if (!name) {
+    res.status(400).json({
+      errorMessage: "Please include a cohort name"
+    });
+  }
+
+  db.insert(post)
+    .then(cohort => {
+      res.status(201).json({
+        message: "Cohort added successfully.",
+        cohort
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error posting to the database."
+      });
+    });
+});
 
 module.exports = router;
--- data/helpers/cohortsModel.js
+++ data/helpers/cohortsModel.js
@@ -1,35 +1,34 @@
-const db = require('../dbConfig');
-
+const db = require("../dbConfig");
 
 module.exports = {
   get,
   getById,
-  getStudentsInCohort
+  getStudentsInCohort,
+  insert
 };
 
 function get() {
   return db("cohorts");
 }
 
-
 function getById(id) {
   return db("cohorts")
-  .where({id})
-  .first();
+    .where({ id })
+    .first();
 }
-
 
 function getStudentsInCohort(id) {
   return db
-  .select('cohorts.name as Cohort','students.name as Name').from('cohorts')
-  .innerJoin('students', 'cohorts.id', 'students.cohort_id')
-  .where('cohorts.id', id)
-    
-    // .where('cohorts.id', id)
-    
-    
-    
+    .select("cohorts.name as Cohort", "students.name as Name")
+    .from("cohorts")
+    .innerJoin("students", "cohorts.id", "students.cohort_id")
+    .where("cohorts.id", id);
+}
 
-  //SELECT c.customerName, c.contactname, o.orderdate  FROM customers c, orders o
-  //where c.customerid = o.customerid
+function insert(post) {
+  return db("cohorts")
+    .insert(post)
+    .then(ids => {
+      return getById(ids[0]);
+    });
 }
